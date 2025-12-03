751b62b1e0e21c0681beb3a18b008bd7802e7a7d
makis <54209831+thioaana@users.noreply.github.com>
2025-12-03 13:11:46 +0000

    final

--- checkClones.py
+++ checkClones.py
@@ -3,7 +3,11 @@ import pandas as pd
 # -------------------------
 # Βήμα 1: Φόρτωση των δύο sheets
 # -------------------------
-excel_file = "contracts.xlsx"
+excel_file = "ZGVTT_GRP_F_TEXT.xlsx"
+# excel_file = "ZGVTt_GRP_F_COM.xlsx"
+# excel_file = "ZGVTt_GRP_F_POL.xlsx"
+# excel_file = "ZGVTT_GRP_F_PRC.xlsx"
+# excel_file = "ZGVTt_GRP_F_PRM.xlsx"
 
 # df_data: το πρώτο sheet με τα δεδομένα
 # df_mapping: το δεύτερο sheet με το mapping
@@ -16,31 +20,28 @@ df_mapping = pd.read_excel(excel_file, sheet_name="Mapping")
 final_side_by_side = []
 
 for _, row in df_mapping.iterrows():
-    old_contract = row["SAMMELN_OLD"]
-    new_contract = row["SAMMELN_NEW"]
+    old_contract = row["SAMMELN_Source"]
+    new_contract = row["SAMMELN_Clone"]
     
     # Φιλτράρουμε γραμμές για κάθε συμβόλαιο
     df_old = df_data[df_data["SAMMELN"] == old_contract].copy()
     df_new = df_data[df_data["SAMMELN"] == new_contract].copy()
+
     
-    # Προσθέτουμε __id για σωστό side-by-side merge
-    df_old["__id"] = df_old.index
-    df_new["__id"] = df_new.index
-    
+    # Στήλες προς συγκριση
+    comparedCols = [c for c in df_data.columns if c!="SAMMELN"]
+
     merged = df_old.merge(
         df_new,
         how="outer",
-        on="__id",
-        suffixes=("_old", "_new"),
+        on=comparedCols,
+        suffixes=("_Source", "_Clone"),
         indicator=True
     )
     
     # Προσθέτουμε στήλες με τα συμβόλαια για αναφορά
-    merged["SAMMELN_OLD"] = old_contract
-    merged["SAMMELN_NEW"] = new_contract
-    
-    # Αφαιρούμε __id
-    merged.drop(columns="__id", inplace=True)
+    merged["SAMMELN_Source"] = old_contract
+    merged["SAMMELN_Clone"] = new_contract
     
     final_side_by_side.append(merged)
 
@@ -53,4 +54,4 @@ final_df = pd.concat(final_side_by_side, ignore_index=True)
 # Βήμα 4: Αποθήκευση σε τρίτο sheet του Excel
 # -------------------------
 with pd.ExcelWriter(excel_file, engine="openpyxl", mode="a") as writer:
-    final_df.to_excel(writer, sheet_name="Side_by_Side", index=False)
+    final_df.to_excel(writer, sheet_name="Result", index=False)
